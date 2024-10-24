ecd7795c8c5f2d504182a1f41123580ee3be1724
HackRoot1 <98283417+HackRoot1@users.noreply.github.com>
2024-10-24 22:45:46 +0530

    added routes to the link

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+
+
+// authentications
+
+Route::get('/register', function () {
     return view('register');
 })->name('register');
+
 Route::get('/login', function () {
     return view('login');
 })->name('login');
@@ -13,27 +18,36 @@ Route::get('/login', function () {
 Route::post('/register-data', [RegisterController::class, 'Store'])->name('register.data');
 Route::post('/login', [RegisterController::class, 'authenticate'])->name('login.page');
 
-Route::get('/home', function () {
+
+
+
+
+
+// pages 
+Route::get('/', function () {
     return view('home');
 })->name('home');
 
-
 Route::get('/single-food', function () {
     return view('single-food');
-});
+})->name('single.food');
+
 Route::get('/checkout-page', function () {
     return view('checkout-page');
-});
+})->name('checkout');
+
 Route::get('/profile', function () {
     return view('profile');
-});
+})->name('profile');
 
 Route::get('/wishlist', function () {
     return view('wishlist');
-});
+})->name('wishlist');
+
 Route::get('/shopping-cart', function () {
     return view('shopping-cart');
-});
+})->name('shopping.cart');
+
 Route::get('/food-page', function () {
     return view('food-page');
-});
+})->name('food.page');
